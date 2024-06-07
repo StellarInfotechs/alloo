f39391cc0163e44e029a2da38876f850c6a0cf88
StellarInfotechs <stellarinfotech71@gmail.com>
2024-06-07 10:25:09 -0700

    Delete humaan directory

--- humaan/_next/static/chunks/app/work/(archive)/page-c95e6f224701ae15.js
+++
@@ -1,8 +0,0 @@
-(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
-    [495, 372], {},
-    function(n) {
-        n.O(0, [14, 332, 203, 726, 987, 919, 971, 824, 744], function() {
-            return n(n.s = 76867)
-        }), _N_E = n.O()
-    }
-]);
